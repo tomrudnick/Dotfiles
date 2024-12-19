baea87dc4525dd74471ad74d3d6f0e3c37b2c6af
tom <tom.rudnick@tum.de>
2024-12-19 15:57:19 +0100

    clean up

--- default.nix
+++ default.nix
@@ -1,36 +1,44 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ config, pkgs, ... }:
+
+let
+  # Custom package imports
+  dmenuExtended = import ./pkgs/dmenu_extended.nix { inherit (pkgs) lib python3Packages; };
+in
 {
-  config,
-  pkgs,
-  ...
-}: let
-  dmenuExtended = import ./pkgs/dmenu_extended.nix {inherit (pkgs) lib python3Packages;};
-in {
+  ####################
+  # Boot and Filesystems
+  ####################
   boot.supportedFilesystems = ["ntfs"];
+  boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      useOSProber = true; # Detect other OSes
+      efiSupport = true;
+    };
+    systemd-boot.enable = false;
+    efi.canTouchEfiVariables = true;
+  };
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true; #finds other system like linux or windows while booting
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  environment.shells = with pkgs; [zsh bash];
+  ####################
+  # User / Shell Configuration
+  ####################
   users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh bash ];
   programs.zsh.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # User Accounts
+  users.users.tom = {
+    isNormalUser = true;
+    description = "Tom Rudnick";
+    extraGroups = ["networkmanager" "wheel" "dialout" "audio" "docker"];
+  };
 
-  # Set your time zone.
+  ####################
+  # Localization
+  ####################
   time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -42,156 +50,155 @@ in {
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "de_DE.UTF-8";
   };
+  console.keyMap = "de";
 
-  services.libinput = {
+  ####################
+  # Networking and Firewall
+  ####################
+  networking.networkmanager.enable = true;
+  networking.firewall = {
     enable = true;
-    mouse = {
-      middleEmulation = false; #disable left + right click = middle click (gaming)
-    };
+    allowedTCPPorts = [ 80 443 53317 8080 ];
+    allowedUDPPortRanges = [
+      { from = 4000; to = 4007; }
+      { from = 53315; to = 53318; }
+      { from = 8000; to = 8010; }
+    ];
   };
 
-  #services.displayManager.defaultSession = "none+i3"; #default is lightdm
-  # Configure keymap in X11
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "de";
-      variant = "";
-    };
-    dpi = 120;
-    desktopManager = {
-      xterm.enable = false;
+  ####################
+  # Services
+  ####################
+  services = {
+    # Input
+    libinput = {
+      enable = true;
+      mouse.middleEmulation = false; # Disable left+right=middle
     };
 
-    windowManager.i3 = {
+    # Display / X Server / Window Manager
+    xserver = {
       enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu
-        i3lock
+      dpi = 120;
+      xkb.layout = "de";
+      xkb.variant = "";
+      desktopManager.xterm.enable = false;
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+        extraPackages = with pkgs; [
+          dmenu
+          i3lock
+        ];
+      };
+    };
+
+    # Audio: PipeWire
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    # Printing
+    printing = {
+      enable = true;
+      drivers = [
+        pkgs.gutenprint
+        pkgs.gutenprintBin
+        pkgs.epson-escpr
       ];
     };
-  };
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  programs.steam = {
-    enable = true;
-  };
-
-  programs.java = {
-    enable = true;
-  };
-
-  services = {
+    # General System Services
     dbus.enable = true;
     gnome.gnome-keyring.enable = true;
     gvfs.enable = true;
     onedrive.enable = true;
+    usbmuxd.enable = true;
+
+    # Ollama
+    ollama = {
+      enable = true;
+      acceleration = "cuda";
+    };
+
+    # Avahi
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
 
+  ####################
+  # Programs
+  ####################
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  programs.steam.enable = true;
+  programs.java.enable = true;
+  programs.dconf.enable = true; # Store GTK3 application settings
+
+  ####################
+  # XDG and Polkit
+  ####################
   xdg.portal.enable = true;
-  xdg.portal.config = {
-    common = {
-      default = [
-        "gtk"
-      ];
-    };
-  };
-
-  programs.dconf.enable = true; #store settings from gtk3 applications like size of file selection dialogs
-
+  xdg.portal.config.common.default = [ "gtk" ];
   security.polkit.enable = true;
-
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-    user.services.dmenuExtendedUpdateDb = {
-      description = "Update dmenu-extended cache";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = "${dmenuExtended}/bin/dmenu_extended_cache_build";
-        Environment = "PATH=/run/current-system/sw/bin";
-      };
-      wantedBy = ["multi-user.target"];
-    };
-    user.timers.dmenuExtendedUpdateDb = {
-      description = "Run dmenu-extended update db service every 5 minutes"; # Adjust the interval as necessary
-      timerConfig.OnCalendar = "*:0/5";
-      wantedBy = ["timers.target"];
-      partOf = ["dmenuExtendedUpdateDb.service"];
-    };
-  };
-
-  # Configure console keymap
-  console.keyMap = "de";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [pkgs.gutenprint pkgs.gutenprintBin pkgs.epson-escpr];
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  # Enable sound with pipewire.
-  #sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # Polkit gnome authentication agent
+  systemd.user.services."polkit-gnome-authentication-agent-1" = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
 
-  services.ollama = {
-    enable = true;
-    acceleration = "cuda";
+  # dmenu-extended update service and timer
+  systemd.user.services.dmenuExtendedUpdateDb = {
+    description = "Update dmenu-extended cache";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${dmenuExtended}/bin/dmenu_extended_cache_build";
+      Environment = "PATH=/run/current-system/sw/bin";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+  systemd.user.timers.dmenuExtendedUpdateDb = {
+    description = "Run dmenu-extended update db service every 5 minutes";
+    timerConfig.OnCalendar = "*:0/5";
+    wantedBy = ["timers.target"];
+    partOf = ["dmenuExtendedUpdateDb.service"];
   };
 
+  ####################
+  # Virtualization / Docker
+  ####################
+  virtualisation.docker.enable = true;
 
-  services.usbmuxd.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.tom = {
-    isNormalUser = true;
-    description = "Tom Rudnick";
-    extraGroups = ["networkmanager" "wheel" "dialout" "audio" "docker"];
-  };
-
-  # Allow unfree packages
+  ####################
+  # Allow Unfree Packages and Nixpkgs Config
+  ####################
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.chromium.enableWideVine = true;
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "mailspring-1.12.0"
-  ];
+  # Nix Settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # a few customizations for nixpkgs
+  # Overlays
   nixpkgs.overlays = [
     (self: super: {
       polybar = super.polybar.override {
@@ -202,162 +209,168 @@ in {
     (self: super: {
       mailspring = super.mailspring.overrideAttrs (oldAttrs: {
         postInstall = ''
-          wrapProgram $out/bin/mailspring --add-flags "--password-store=gnome-libsecret" #otherwise passwords can't be stored
+          wrapProgram $out/bin/mailspring --add-flags "--password-store=gnome-libsecret"
         '';
       });
     })
   ];
 
-  nixpkgs.config.chromium.enableWideVine = true;
+  ####################
+  # Hardware / Graphics
+  ####################
+  hardware.pulseaudio.enable = false;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
-  environment.systemPackages = with pkgs; [
-    gnumake
-    gcc
-    glibc
-    clang
-    cmake
-    zip
-    unzip
-    usbutils
-    killall
-    tree
-    go
-    wget
-    firefox
-    kitty
-    sxiv #replacement for feh
-    zathura
-    networkmanagerapplet
-    picom
-    nitrogen
-    google-chrome
-    _1password-gui
-    polkit_gnome
-    pcmanfm
-    pulseaudio # to get command line tools (pactl)
-    pavucontrol
-    playerctl
-    gparted # best disk formatter
-    spotify
-    discord
-    arandr
-    evince
-    shared-mime-info
-    lxmenu-data
-    (python3.withPackages (ps: with ps; [pandas requests dbus-python numpy pip]))
-    jetbrains.idea-ultimate
-    github-copilot-intellij-agent #this is rather a hotfix since I symlink from the /run/current-system to intellij
-    jetbrains.rust-rover
-    jetbrains.pycharm-professional
-    jetbrains.rider
-    jetbrains.webstorm
-    texliveFull
-    libreoffice
-    hunspell
-    hunspellDicts.de_DE
-    vscode
-    baobab
-    libnotify
-    dunst
-    polybar
-    lxappearance
-    btop
-    eza
-    bat
-    fzf
-    neofetch
-    ranger
-    (import ./pkgs/dmenu_extended.nix {inherit (pkgs) lib python3Packages;})
-    flameshot #best screenshot tool
-    teamspeak_client
-    neovim
-    jetbrains.goland
-    arp-scan
-    arduino
-    kicad
-    prusa-slicer
-    zoom-us
-    sl
-    sox
-    lolcat
-    gpu-screen-recorder
-    vlc
-    gimp
-    mate.engrampa #compressed file viewer (good integration with pcmanfm)
-    chromium
-    filezilla
-    rpi-imager
-    sage
-    qjackctl
-    ffmpeg
-    yt-dlp
-    audacity
-    mailspring
-    localsend
-    #geogebra6
-    geogebra
-    manim
-    tor-browser-bundle-bin
-    nh
-    lua-language-server
-    stylua
-    marksman
-    alejandra
-    nixd
-    ltex-ls
-    nodePackages.bash-language-server
-    texlab
-    ripgrep
-    xclip
-    postman
-    glibc
-    patchelf
-    grpc-tools
-    github-desktop
-    ngrok
-    nodejs
-    imagemagickBig
-    uv
-    typst-lsp
-    typst
-    nautilus
-  ];
-
+  ####################
+  # Fonts
+  ####################
   fonts.packages = with pkgs; [
     nerdfonts
     font-awesome
     siji
   ];
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
+  ####################
+  # System Packages
+  ####################
+  environment.systemPackages = with pkgs; [
+    # Development Tools - Programming Languages
+    gnumake
+    gcc
+    glibc
+    clang
+    cmake
+    go
+    (python3.withPackages (ps: with ps; [ pandas requests dbus-python numpy pip pygobject3 ]))
+    nodejs
+    uv
 
-  virtualisation.docker.enable = true;
+    # Development Tools - Utilities
+    ripgrep
+    fzf
+    bat
+    eza
+    btop
+    nh
+    patchelf
 
-  #this is neccessary cause of localsend
-  networking = {
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [80 443 53317 8080];
-      allowedUDPPortRanges = [
-        {
-          from = 4000;
-          to = 4007;
-        }
-        {
-          from = 53315;
-          to = 53318;
-        }
-        {
-          from = 8000;
-          to = 8010;
-        }
-      ];
-    };
-  };
+    # Development Tools - Editors / IDEs
+    neovim
+    ranger
+    marksman
+    alejandra
+    nixd
+    ltex-ls
+    lua-language-server
+    stylua
+    nodePackages.bash-language-server
+    texlab
+    grpc-tools
+    ngrok
 
-  system.stateVersion = "23.11"; # Don't change
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+    # Browsers
+    firefox
+    google-chrome
+    chromium
+    tor-browser-bundle-bin
+
+    # IDEs
+    jetbrains.idea-ultimate
+    github-copilot-intellij-agent
+    jetbrains.rust-rover
+    jetbrains.pycharm-professional
+    jetbrains.webstorm
+    vscode
+
+    # Desktop Tools - Image and Media
+    sxiv
+    zathura
+    picom
+    nitrogen
+    flameshot
+    gimp
+    vlc
+    ffmpeg
+    gpu-screen-recorder
+    imagemagickBig
+    audacity
+
+    # Desktop Tools - Productivity
+    kitty
+    _1password-gui
+    polkit_gnome
+    pcmanfm
+    nautilus
+    spotify
+    discord
+    teamspeak_client
+    filezilla
+    geogebra
+    mailspring
+    postman
+    github-desktop
+    gparted
+    qjackctl
+    prusa-slicer
+    rpi-imager
+
+    # Development Tools - Hardware
+    arduino
+    kicad
+
+    # Office Tools
+    libreoffice
+    hunspell
+    hunspellDicts.de_DE
+    texliveFull
+    typst
+    typst-lsp
+
+    # Desktop Utilities
+    mate.engrampa
+    waybar
+    zip
+    unzip
+    usbutils
+    killall
+    tree
+    wget
+    networkmanagerapplet
+    playerctl
+    pavucontrol
+    shared-mime-info
+    lxmenu-data
+    arandr
+    evince
+    baobab
+    libnotify
+    dunst
+    polybar
+    lxappearance
+
+    # Fun and Miscellaneous
+    sl
+    sox
+    neofetch
+    lolcat
+
+    # Networking
+    arp-scan
+    localsend
+
+    # Communication Tools
+    zoom-us
+
+    # Launcher and Workflow Tools
+    dmenuExtended # Imported custom package
+
+    # Mathematical Tools
+    sage
+  ];
+
+
+  system.stateVersion = "23.11"; # Do not change
 }
